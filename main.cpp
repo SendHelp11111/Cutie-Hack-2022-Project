bcb47bed335399afed9a897941f4a048b64ce66d
dzshao <danielzhshao@gmail.com>
2022-11-05 17:42:05 -0700

    Added libraries needing for plotting graphs and created display() function in main. Display function parameters likely to be changed

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 #include <vector>
 #include "EnergySource.h"
 #include "TotalEnergy.h"
+#include "pbPlots.hpp"
+#include "supportLib.hpp"
 
 using namespace std;
+
+void display(vector<double> &years, vector<double> &energyProduction);
 
 int main(int argc, char* argv[]){
     // vector <string> names {"Coal Production", "Natural Gas (Dry)", "Crude Oil Production", "Natural Gas (Liquid)", "Total Fossil Fuel Production"
@@ -45,6 +49,43 @@ int main(int argc, char* argv[]){
     for(unsigned int i = 0; i < current.getArr().size(); i++){
          cout << fixed << setprecision(3) << current.getArr().at(i);
     }
-
+    vector<double> x;
+    for(int i = 0; i < 10; ++i) {
+        x.push_back(i*2.5);
+    }
+    vector<double> y;
+    for(int i = 0; i < 10; ++i) {
+        y.push_back(i*3.7);
+    }
+    display(x, y);
     return 0; 
+}
+
+void display(vector<double> &years, vector<double> &energyProduction) {
+    bool success;
+    StringReference *errorMessage;
+    RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
+
+    ScatterPlotSeries *series = GetDefaultScatterPlotSeriesSettings();
+    series->xs = &years;
+    series->ys = &energyProduction;
+    series->color = CreateRGBColor(0,0,1);
+
+    ScatterPlotSettings *settings = GetDefaultScatterPlotSettings();
+    settings->title = toVector(L"test");
+    settings->width = 900;
+    settings->height = 600;
+    settings->xLabel = toVector(L"Year");
+    settings->yLabel = toVector(L"Energy Production");
+    settings->scatterPlotSeries->push_back(series);
+
+    success = DrawScatterPlotFromSettings(imageRef, settings, errorMessage);
+
+    if (success) {
+        vector<double> *pngdata = ConvertToPNG(imageRef->image);
+        WriteToFile(pngdata, "test.png");
+        DeleteImage(imageRef->image);
+    } else {
+        cout << "Error drawing plot";
+    }
 }
