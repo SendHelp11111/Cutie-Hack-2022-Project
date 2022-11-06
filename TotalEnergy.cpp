b177b4e530330d5fd6c909c80f6796cc5e77bc86
Splinter003 <sazen2011@gmail.com>
2022-11-05 20:36:44 -0700

    update to total energy

--- TotalEnergy.cpp
+++ TotalEnergy.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 
 using namespace std;
+TotalEnergy::TotalEnergy(){
 
-TotalEnergy::TotalEnergy() {
-
+}
+TotalEnergy::TotalEnergy(int year) {
+    int _year = year;
 }
 
 vector<EnergySource> TotalEnergy::getArr() {
@@ -19,42 +21,90 @@ void TotalEnergy::add(EnergySource n) {
 
 void TotalEnergy::print() {
     for(unsigned int i = 0; i < arr.size(); ++i) {
-        cout << fixed << setprecision(3) << arr.at(i) << endl;
+        cout << arr.at(i) << endl;
     }
 }
 
-double TotalEnergy::sumValCol(int col) {
-    double sum = 0.0;
+//functions for average
+
+int TotalEnergy::sumYear(int year) {
+    int sum = 0; 
+    for(unsigned int i = 0; i < arr.size(); i++){
+        if(arr.at(i).getYear() >= year){
+            sum += arr.at(i).getYear();
+        }
+    }
+    return sum;
+}
+
+double TotalEnergy::sumValue(int year) {
+    int sum = 0;
     for(unsigned int i = 0; i < arr.size(); ++i) {
-        if(arr.at(i).getOrder() == col) {
+        if(arr.at(i).getYear() >= year) {
             sum += arr.at(i).getValue();
         }
     }
     return sum;
 }
 
-double TotalEnergy::sumPerYear(int year) {
-    double sum = 0.0;
+int TotalEnergy::totalYearsAfter(int year) {
+    int sum = 0;
     for(unsigned int i = 0; i < arr.size(); ++i) {
-        if(arr.at(i).getYear() == year) {
-            sum += arr.at(i).getValue();
+        if(arr.at(i).getYear() >= year) {
+            sum ++;
         }
     }
-    return sum;    
+    return sum;
 }
 
-vector <double> TotalEnergy::vectorYears(){
-    vector<double> temp;
-    for(unsigned int i = 0; i < arr.size(); i++){
-        temp.push_back(arr.at(i).getYear());
-    }
-    return temp;
+int TotalEnergy::avgYear(int year) {return (sumYear(year)/totalYearsAfter(year));}
+
+double TotalEnergy::avgValue(int year) {return (sumValue(year)/totalYearsAfter(year));}
+
+//functions for regression line
+int TotalEnergy::sumYearFromAvg(int year) {
+    int sum = 0;
+    for(unsigned int i = 0; i < arr.size(); ++i) {
+        if(arr.at(i).getYear() > year) {
+            sum += (arr.at(i).getYear() - avgYear(year));
+        }
+    }    
+    return sum;
 }
-vector <double> TotalEnergy::vectorValue(){
-    vector<double> temp;
-    for(unsigned int i = 0; i < arr.size(); i++){
-        temp.push_back(arr.at(i).getValue());
-    }
-    return temp;
+
+double TotalEnergy::sumValueFromAvg(int year) {
+    int sum = 0;
+    for(unsigned int i = 0; i < arr.size(); ++i) {
+        if(arr.at(i).getYear() > year) {
+            sum += (arr.at(i).getValue() - avgValue(year));
+        }
+    }    
+    return sum;
 }
+
+void TotalEnergy::regression(int year) {
+    double top = sumValueFromAvg(year) * sumYearFromAvg(year);
+    double bot = sumYearFromAvg(year) * sumYearFromAvg(year);
+    _regression = top/bot;
+}
+
+double TotalEnergy::getReg() {return _regression;}
+
+void TotalEnergy::yInt(int year) {
+    regression(year);
+    _yInt = (avgValue(year) - (avgYear(year)*_regression));
+}
+
+double TotalEnergy::getYInt() {return _yInt;}
+
+void TotalEnergy::removeZero() {
+    for(unsigned int i = 0; i < arr.size(); ++i) {
+        if(arr.at(i).getValue() == 0) {
+            arr.erase(arr.begin()+i);
+        }
+    }
+}
+
+
+
 
